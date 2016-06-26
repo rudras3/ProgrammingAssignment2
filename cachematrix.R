0ecdd8d3f33b66b1e70e2b424f9d70c6186e8ac1
rudras3 <srudrava@gmail.com>
2016-06-25 22:49:06 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeMatrix <- function(x = matrix()) {
+   ## By Sunil Rudravajhala for Assignment 3 2016/06/25
+	## function creates list containing functions for set,get value of matrix and set,get 
+	##  matrix inverse
+	m <- NULL
+	set <- function(y) {
+		 x <<- y
+		 m <<- NULL
+	}
+	get <-function() x
+	setmatrixinv <- function(inverse) m <<- inverse
+	getmatrixinv <- function() m
+	list(set=set, get=get, setmatrixinv=setmatrixinv, getmatrixinv=getmatrixinv)
 }
+cachematrixinv <- function(x, ...) {
+	## this function calculates the inverse of a matrix and checks if its cached first
+	m = x$getmatrixinv()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setmatrixinv(m)
+	m
+}
+testcache = function(m){
+	## to test functions above
+	tempm=makeMatrix(m)
+	start = Sys.time()
+	cachematrixinv(tempm)
+	end=Sys.time()
+	diff = end - start
+	print(diff)
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	start = Sys.time()
+	cachematrixinv(tempm)
+	end=Sys.time()
+	diff = end - start
+	print(diff)	
 }
